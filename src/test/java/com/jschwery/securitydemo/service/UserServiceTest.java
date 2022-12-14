6e5dd922be361da3389b5e9a45eee58961fc2a9e
Jschwery <86555316+Jschwery@users.noreply.github.com>
2022-12-14 06:22:14 -0600

    com 1

--- src/test/java/com/jschwery/securitydemo/service/UserServiceTest.java
+++ src/test/java/com/jschwery/securitydemo/service/UserServiceTest.java
@@ -5,68 +5,46 @@ import com.jschwery.securitydemo.entities.User;
 import com.jschwery.securitydemo.model.UserModel;
 import com.jschwery.securitydemo.repository.UserRepository;
 import com.jschwery.securitydemo.service.Implementations.UserServiceImpl;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.Assert.assertEquals;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Mock
-    UserRepository repository;
+    private UserRepository repository;
+
     @InjectMocks
-    UserServiceImpl userServiceImpl;
+    private UserServiceImpl userServiceImpl;
 
-    UserModel inputUserModel;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        inputUserModel = new UserModel();
+    @Test
+    void saveUser_userHasID(){
+        // Arrange
+        final UserModel inputUserModel = new UserModel();
         inputUserModel.setEmail("testemail@gmail.com");
         inputUserModel.setFirstName("john");
         inputUserModel.setLastName("doe");
         inputUserModel.setPassword("test");
         inputUserModel.setMatchPassword("test");
 
-        User inputUser = User.builder()
-                .email(inputUserModel.getEmail())
-                .firstName(inputUserModel.getFirstName())
-                .lastName(inputUserModel.getLastName())
-                .password(inputUserModel.getPassword())
-                .timeCreated(Timestamp.valueOf(LocalDateTime.now(ZoneId.systemDefault()))).userID(1)
-                .build();
+        when(repository.save(any(User.class))).thenAnswer(invocation -> {
+            final User entity = invocation.getArgument(0);
+            ReflectionTestUtils.setField(entity, "userID", 1);
+            return entity;
+        });
 
-        User outputUser = User.builder().
-                email(inputUserModel.getFirstName()).
-                password(inputUserModel.getLastName()).
-                lastName(inputUserModel.getFirstName())
-                .firstName(inputUserModel.getFirstName())
+        final User user = userServiceImpl.saveUser(inputUserModel);
+        System.out.println(user.getUserID());
 
-                .timeCreated(Timestamp.valueOf(LocalDateTime.now(ZoneId.systemDefault()))).userID(1).build();
+        assertThat(user.getUserID()).isEqualTo(1);
 
-        Mockito.when(repository.save(inputUser)).thenReturn(outputUser);
-
-    }
-
-    @Test
-    public void whenSaveUser_ThenUserHasID(){
-
-        Assertions.assertEquals(1, userServiceImpl.saveUser(inputUserModel).getUserID());
     }
 }
