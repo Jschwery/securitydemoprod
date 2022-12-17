4a5c82ea26f7bcb0da22a6ccaddd8788b84df033
jared <jaredschwery1@gmail.com>
2022-12-17 05:43:51 -0600

    commit 2 - created user token and verification

--- src/main/java/com/jschwery/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/jschwery/securitydemo/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,6 +27,21 @@ import javax.xml.crypto.Data;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+/*
+if the user is an admin they can remove other users from db
+plus all other features of users
+
+HTTP request -> security filter chain
+filter chain -> custom http login page
+same thymeleaf html page has get and post available to post the form details?
+the thymeleaf page should have the oauth google link too
+if user clicks on the google link, provider manager
+
+
+
+
+ */
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
@@ -37,11 +54,16 @@ public class SecurityConfig {
     @Autowired
     DataSource dataSource;
 
+    public void authProvider1(AuthenticationProvider oAuthProvider){
+
+    }
+
     /**
      * Authenticates with the specified database in the properties file
      * finds the user from the table by username we can get their information and find their roles to assign
      * proper authorization
      */
+    @Bean
     public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
         authBuilder.jdbcAuthentication()
                 .passwordEncoder(getPassWordEncoder())//spring security requires these columns to be "username, enabled, password & role"
@@ -67,7 +89,11 @@ public class SecurityConfig {
 
     //only used for the password because its only authentication where we store the data in the database
     //this implements authentication manager
-    ProviderManager manager = new ProviderManager();
+    @Bean
+    public ProviderManager getProviderManager(){
+        return new ProviderManager();
+    }
+    //loop through
 
     //how do we go from the security filter chain to the provider manager
 
@@ -75,7 +101,7 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, RememberMeServices rememberme) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).
                 formLogin(form -> form.loginPage("/login"));
         return http.build();
