ea435268b17c737e5086f747404e12fd78075c6b
Jschwery <86555316+Jschwery@users.noreply.github.com>
2023-01-02 07:26:42 -0600

    last commit with thymeleaf

--- src/main/java/com/jschwery/securitydemo/entities/UserRegistrationForm.java
+++ src/main/java/com/jschwery/securitydemo/entities/UserRegistrationForm.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -22,14 +23,19 @@ public class UserRegistrationForm {
     Long registerFormID;
 
     @NotNull(message = "Username is required")
+    @NotEmpty(message = "Username is required")
     @Size(min = 3, max = 16, message = "Username must be between 3 and 16 characters")
     private String username;
     @NotNull(message = "Please enter a password")
+    @NotEmpty(message = "Password is required")
     private String password;
+    @NotEmpty(message = "Re-enter password is required")
     private String matchPassword;
     @NotNull(message = "First name is required")
+    @NotEmpty(message = "Re-enter password is required")
     private String firstName;
     @NotNull(message = "Last name is required")
+    @NotEmpty(message = "Re-enter password is required")
     private String lastName;
     @NotNull(message = "Email is required")
     private String email;
